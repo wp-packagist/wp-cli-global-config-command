85ec56deae8009e93a284fbf1dfe08a7116bf772
Mehrshad Darzi <mehrshad198@gmail.com>
2019-04-28 19:44:18 +0430

    init commit

---
+++ command.php
@@ -0,0 +1,9 @@
+<?php
+
+# Check Exist WP-CLI
+if ( ! class_exists( 'WP_CLI' ) ) {
+	return;
+}
+
+# Register 'global-config' Command
+WP_CLI::add_command( 'global-config', 'Global_Config_Command' );
